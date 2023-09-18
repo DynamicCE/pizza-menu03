39086ae1ea8da1aa6b509dd3675e3f4184bc4bae
Adınız <erkanmurat0632@gmail.com>
2023-09-18 18:54:07 +0300

    footer component

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+function Footer() {
+  const hour = new Date().getHours(); // getHours renders a number between 0 - 23
+  const openHour = 12;
+  const closeHour = 2;
+
+  let isOpen;
+
+  if (openHour < closeHour) {
+    // Normal durum: Açılış ve kapanış saatleri aynı gün içinde
+    isOpen = hour >= openHour && hour < closeHour;
+    /* eğer açılış saati, kapanış saatinden küçükse yani mesela 12 de açılıp 23'de kapatıyorsak, 
+      saatimizin açılış saatinden büyük ya da eşit olması veya saatimizin kapanış saatinden önce olması Restoranın açık olduğunu gösterir
+      */
+  } else {
+    // Gece yarısını geçen durum: Açılış ve kapanış saatleri farklı günlerde
+    // eğer kapanış saati daha küçükse mesela 12 de açıp 2 de kapatıyorsak; saatimizin açılış saatinden yine büyük olması veya
+    // kapanış saatinden küçük olması lazım ki restoran açık olabilsin.
+    isOpen = hour >= openHour || hour < closeHour;
+  }
+
+  if (isOpen) {
+    alert("we are currently open! :)");
+  } else {
+    alert("Sorry, we are closed :(");
+  }
+  return (
+    <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+//components
+import Footer from "./components/footer";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -77,35 +79,35 @@ function Menu() {
   );
 }
 
-function Footer() {
-  const hour = new Date().getHours(); // getHours renders a number between 0 - 23
-  const openHour = 12;
-  const closeHour = 2;
+// function Footer() {
+//   const hour = new Date().getHours(); // getHours renders a number between 0 - 23
+//   const openHour = 12;
+//   const closeHour = 2;
 
-  let isOpen;
+//   let isOpen;
 
-  if (openHour < closeHour) {
-    // Normal durum: Açılış ve kapanış saatleri aynı gün içinde
-    isOpen = hour >= openHour && hour < closeHour;
-    /* eğer açılış saati, kapanış saatinden küçükse yani mesela 12 de açılıp 23'de kapatıyorsak, 
-    saatimizin açılış saatinden büyük ya da eşit olması veya saatimizin kapanış saatinden önce olması Restoranın açık olduğunu gösterir
-    */
-  } else {
-    // Gece yarısını geçen durum: Açılış ve kapanış saatleri farklı günlerde
-    // eğer kapanış saati daha küçükse mesela 12 de açıp 2 de kapatıyorsak; saatimizin açılış saatinden yine büyük olması veya
-    // kapanış saatinden küçük olması lazım ki restoran açık olabilsin.
-    isOpen = hour >= openHour || hour < closeHour;
-  }
+//   if (openHour < closeHour) {
+//     // Normal durum: Açılış ve kapanış saatleri aynı gün içinde
+//     isOpen = hour >= openHour && hour < closeHour;
+//     /* eğer açılış saati, kapanış saatinden küçükse yani mesela 12 de açılıp 23'de kapatıyorsak,
+//     saatimizin açılış saatinden büyük ya da eşit olması veya saatimizin kapanış saatinden önce olması Restoranın açık olduğunu gösterir
+//     */
+//   } else {
+//     // Gece yarısını geçen durum: Açılış ve kapanış saatleri farklı günlerde
+//     // eğer kapanış saati daha küçükse mesela 12 de açıp 2 de kapatıyorsak; saatimizin açılış saatinden yine büyük olması veya
+//     // kapanış saatinden küçük olması lazım ki restoran açık olabilsin.
+//     isOpen = hour >= openHour || hour < closeHour;
+//   }
 
-  if (isOpen) {
-    alert("we are currently open! :)");
-  } else {
-    alert("Sorry, we are closed :(");
-  }
-  return (
-    <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
-  );
-}
+//   if (isOpen) {
+//     alert("we are currently open! :)");
+//   } else {
+//     alert("Sorry, we are closed :(");
+//   }
+//   return (
+//     <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
+//   );
+// }
 
 function Pizza() {
   return (
